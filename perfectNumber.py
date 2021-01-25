bc62d8d6f368bca1578f9139aa0736a5101de9aa
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2021-01-25 19:02:08 +0300

    Update perfectNumber.py

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,11 +1,13 @@
-a = int(input("Please enter the number you want:"))
-i = 1
-sum = 0
-while(i < a):
-  if(a % i == 0):
-    sum += i
-  i += 1
-if(sum == a):
-  print(a," is a perfect number")
-else:
-  print(a," is not a perfect number")
+def perfect():
+  a = int(input("Please enter the number you want:"))
+  i = 1
+  sum = 0
+  while(i < a):
+    if(a % i == 0):
+      sum += i
+    i += 1
+  if(sum == a):
+    print(a," is a perfect number")
+  else:
+    print(a," is not a perfect number")
+perfect()
