f8123181de893679ce92b74cf628eed1006d2d54
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2021-01-27 22:23:02 +0300

    Update basicCalculator.py

--- basicCalculator.py
+++ basicCalculator.py
@@ -3,7 +3,7 @@ b = int(input("Please enter second number:"))
 
 print("For addition select 1\nFor subtract select 2\nFor multiplication select 3\nFor division select 4\n")
 
-operation =  input("Please enter the operation number:") # Buna göre koşullarımızı yazacağız.
+operation =  input("Please enter the operation number:") 
 
 if (operation == "1"):
     print("{} + {} = {}".format(a , b , a + b))
