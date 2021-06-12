b220c4f05d0d99ee5d761c370c9e40fe1c25c266
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2021-06-12 21:58:15 +0300

    Update primeNumber.java

--- primeNumber.java
+++ primeNumber.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1 ile 100 arasındaki asal sayılar");
         int flag = 0;
-        for(int number = 1; number <= 100; number++)
+        for(int number = 2; number <= 100; number++)
         {
             int f2 = 0;
             for (int i = 2; i < number; i++)
